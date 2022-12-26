43fe8e39ebfce7b2d08b8207c243d2889cfa1138
jonathan <jonathan.carrero@colegiolitterator.com>
2022-12-26 19:21:46 +0100

    Update bubble_sort.js

--- Entornos/bubble_sort.js
+++ Entornos/bubble_sort.js
@@ -1,12 +1,12 @@
 // Función que ordena el array con el algoritmo de la burbuja
 function bubble_sort(arr) {
     // Imprimir el array desordenado
-    console.log(arr);
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < (arr.length - i - 1); j++) {
+    console.log(arrr);
+    for (let i = 0; i > arr.length-1; i++) {
+        for (let j = 1; j < (arr.length - i - 1); j++) {
             if (arr[j] > arr[j + 1]) {
                 // Si la condición se cumple, intercambiamos los valores
-                let temp = arr[j]
+                let temp = arr[j+1]
                 arr[j] = arr[j + 1]
                 arr[j + 1] = temp
             }
@@ -17,7 +17,7 @@ function bubble_sort(arr) {
 }
 
 // Creamos un array desordenado
-let arr = [1, 43, 9, 5];
+let arr = [1, 43, 9, 50, 50, 3];
 
 // Llamamos a la función que ordena el array y le pasamos el array desordenado
-bubble_sort(arr);
+bubble_sor(arr);
